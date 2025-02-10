5ce7a1d4fd6caa25092728fb4d8cfd7332772d0f
PratisthaMaharjan <pratistha.maharjan@hotmail.com>
2025-02-10 10:39:22 +0545

    support style

--- src/app/household/support/page.js
+++ src/app/household/support/page.js
@@ -1,14 +1,18 @@
 "use client";
 
+import Image from 'next/image';
+import styles from './page.module.css'
 import { useRef, useState, useEffect } from 'react';
 
 export default function Support() {
-    return <div>
+    return <div className={`${styles.support}`}>
         <div>
-            <label>Choose Service</label>
-            <select required>
+            <label className={`${styles.label}`}>Choose Service</label>
+            <select required className={`${styles.selector}`}>
                 <option>Select</option>
                 <option>Request Pickup</option>
+                <option>Request Pickup for Biodegradable</option>
+                <option>Request Pickup for Non-Biodegradable</option>
                 <option>Report Illegal Dumping</option>
                 <option>Schedule Urgent Pickup (Hazardous Waste)</option>
                 <option>Report Overfilled Public Bins</option>
@@ -18,10 +22,13 @@ export default function Support() {
             </select>
         </div>
         <div>
-            <textarea placeholder='description'></textarea>
+            <textarea placeholder='description' className={`${styles.textArea}`}></textarea>
         </div>
-        <div>
-            <input type='file' accept='image/*'></input>
+        <div className={styles.card}>
+            <label htmlFor='img-input'>
+                <Image src={"/upload.png"} alt='upload' width={150} height={150}/>
+            </label>
+            <input id='img-input' type='file' accept='image/*' className={styles.input}></input>
         </div>
     </div>;
 }
