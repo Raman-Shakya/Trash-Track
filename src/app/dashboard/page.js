25e4b84386db040220bdf9132fd0a8d9438baa41
PratisthaMaharjan <pratistha.maharjan@hotmail.com>
2025-02-10 17:13:24 +0545

    dashboard folder created

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from './page.module.css'
+import { useRef, useState, useEffect } from 'react';
+
+export default function Dashboard() {
+    return <div className={styles.card}>
+        <div  className={styles.cardContent}>
+            <p>sdfhjkdjasldklfhkjdhsaksdjlakjdiowerfhufejdklasdpowedkijdfskofpofdgpdrfsfkisujdfidfiorksasjdsdifidfckoldslp;</p>
+        </div>
+    </div>;
+}
