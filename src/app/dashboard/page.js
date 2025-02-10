6aa561b30b8c61439c624fef63c8b415bcf2277c
PratisthaMaharjan <pratistha.maharjan@hotmail.com>
2025-02-10 19:41:08 +0545

    card component

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,14 @@
 "use client";
 
+import Card from './components/card'
 import styles from './page.module.css'
 import { useRef, useState, useEffect } from 'react';
 
 export default function Dashboard() {
     return <div className={styles.card}>
-        <div  className={styles.cardContent}>
-            <p>sdfhjkdjasldklfhkjdhsaksdjlakjdiowerfhufejdklasdpowedkijdfskofpofdgpdrfsfkisujdfidfiorksasjdsdifidfckoldslp;</p>
-        </div>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
     </div>;
 }
